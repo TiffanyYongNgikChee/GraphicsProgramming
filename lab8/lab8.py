976f9e671bede2dbddefb98d5c449a84376d86e8
tiffanyyong <G00425067@atu.ie>
2024-11-19 22:12:18 +0000

    3x3,5x5,9x9,13x13GaussianBlur

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -8,25 +8,52 @@ img = cv2.imread('ATU.jpg',)
 # Convert the image to greyscale
 gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-# Display the images
-cv2.imshow('Original Image', img)
-cv2.imshow('Grayscale Image', gray_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Apply Gaussian Blur with different kernel sizes
+blur_3x3 = cv2.GaussianBlur(gray_image, (3, 3), 0)
+blur_5x5 = cv2.GaussianBlur(gray_image, (5, 5), 0)
+blur_9x9 = cv2.GaussianBlur(gray_image, (9, 9), 0)
+blur_13x13 = cv2.GaussianBlur(gray_image, (13, 13), 0)
+
+# Plot the results
+plt.figure(figsize=(12, 8))
 
 # Plot using Matplotlib
 nrows, ncols = 2, 1
 
 # Plot the original image
-plt.subplot(nrows, ncols, 1)
+plt.subplot(2, 3, 1)
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))  # Convert BGR to RGB
 plt.title('Original Image')
 plt.xticks([]), plt.yticks([])
 
 # Plot the grayscale image
-plt.subplot(nrows, ncols, 2)
+plt.subplot(2, 3, 2)
 plt.imshow(gray_image, cmap='gray')
 plt.title('Grayscale Image')
+plt.xticks([]), plt.yticks([])
+
+# 3x3 Gaussian Blur
+plt.subplot(2, 3, 3)
+plt.imshow(blur_3x3, cmap='gray')
+plt.title('3x3 Kernel')
+plt.xticks([]), plt.yticks([])
+
+# 5x5 Gaussian Blur
+plt.subplot(2, 3, 4)
+plt.imshow(blur_5x5, cmap='gray')
+plt.title('5x5 Kernel')
+plt.xticks([]), plt.yticks([])
+
+# 9x9 Gaussian Blur
+plt.subplot(2, 3, 5)
+plt.imshow(blur_9x9, cmap='gray')
+plt.title('9x9 Kernel')
+plt.xticks([]), plt.yticks([])
+
+# 13x13 Gaussian Blur
+plt.subplot(2, 3, 6)
+plt.imshow(blur_13x13, cmap='gray')
+plt.title('13x13 Kernel')
 plt.xticks([]), plt.yticks([])
 
 # Show the plots
