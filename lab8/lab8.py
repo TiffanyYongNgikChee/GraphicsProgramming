288e2c5c37dff47e3bb5f1e1975f9d8c77595653
TiffanyYongNgikChee <G00425067@atu.ie>
2024-11-19 10:25:35 +0000

    convert ATU image to grayscale

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -1,3 +1,16 @@
 import cv2
-import numpy as np
+import numpy as np 
 from matplotlib import pyplot as plt
+
+# Load the input image
+img = cv2.imread('ATU.jpg',)
+#cv2.imshow('Original', img)
+#cv2.waitKey(0)
+
+# Convert the image to greyscale
+gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# Display the grayscale image
+cv2.imshow('Grayscale Image',gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
