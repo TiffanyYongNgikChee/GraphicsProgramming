ab6f916d57d2ed84d6e8a35e77b027a7251b8f4c
tiffanyyong <G00425067@atu.ie>
2024-11-26 19:06:08 +0000

    Ad part: object tracking & count

--- lab9/lab9_part2.py
+++ lab9/lab9_part2.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLOv8n model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8m.pt')
 
 # Run inference on the source
-results = model(source="car_race.mp4",show=True,conf=0.4,save=True)
+results = model.track(source='car_race.mp4',show=True, tracker="bytetrack.yaml")
