f3fc4de8415a72eac73338920f1454dc6e9cad3b
tiffanyyong <G00425067@atu.ie>
2024-11-26 18:57:37 +0000

    fixed some issues

--- lab9/lab9_part2.py
+++ lab9/lab9_part2.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 # Run inference on the source
-results = model(source="race.mp4",show=True,conf=0.4,save=True)
+results = model(source="car_race.mp4",show=True,conf=0.4,save=True)
