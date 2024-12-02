b5d32abcfbd5c4827a6aa5f543a3ed739ce71665
tiffanyyong <G00425067@atu.ie>
2024-12-02 09:13:22 +0000

    Ad part: constrain the algorithm

--- lab9/lab9_part2.py
+++ lab9/lab9_part2.py
@@ -1,7 +1,10 @@
 from ultralytics import YOLO
 
+# Detect objects from classes car only
+classes = [2,3,4,5]
+
 # Load a pretrained YOLOv8n model
 model = YOLO('yolov8m.pt')
 
 # Run inference on the source
-results = model.track(source='car_race.mp4',show=True, tracker="bytetrack.yaml")
+results = model.track(source='car_races.mp4',show=True, classes=classes,tracker="bytetrack.yaml")
